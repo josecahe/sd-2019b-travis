8903578c754edd4408c403f815f81f02eb4e6df1
Sebastian Calero <john.calero@correo.icesi.edu.co>
2019-10-02 19:03:43 -0500

    add some test

--- test_demo.py
+++ test_demo.py
@@ -5,7 +5,6 @@ def test_add():
 # GIVEN two numbers
 # WHEN I invoke the method
 # THEN I get the addition of two numbers
-    add_result = demo.add(3,4)
-    assert add_result == 7
-
-
+    assert demo.add(3,4) == 7
+    assert demo.add(1,9) == 10
+    assert demo.add(2,4) == 6
